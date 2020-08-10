83cc25522b9c66254eecf5d4b80c35641e63d8a4
Milos Peric <steamestt@gmail.com>
2020-08-10 12:37:34 +0200

    +Add project files

---
+++ Nedeljni_II_Milos_Peric/Nedeljni_II_Milos_Peric/ViewModel/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nedeljni_II_Milos_Peric.ViewModel
+{
+    class LoginViewModel
+    {
+    }
+}
